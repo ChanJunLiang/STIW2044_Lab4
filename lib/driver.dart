b37d3f470e62cf20a861e874664240e87ea55efa
Liang <cjunliang971023@hotmail.com>
2019-11-28 16:52:55 +0800

    lab4

---
+++ lib/driver.dart
@@ -0,0 +1,5 @@
+class Driver {
+  String name, email, phone ;
+
+  Driver({this.name, this.email, this.phone});
+}
---
+++ lib/job.dart
@@ -0,0 +1,18 @@
+class Job {
+  String jobid,
+      jobcust,
+      joblocation,
+      jobdestination,
+      jobcontact,
+      jobaccept;
+      
+
+  Job(
+      {this.jobid,
+      this.jobcust,
+      this.joblocation,
+      this.jobdestination,
+      this.jobcontact,
+      this.jobaccept,
+      });
+}
